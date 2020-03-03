9665240e8be9b8bcc2bba661085bda2bdd910d05
Chow Jia Ying <chowjiaying211@gmail.com>
2020-03-03 12:39:46 +0800

    Integrate React Router with ReactStrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import Header from "./components/NavBar/Header";
 
 import CreateToDo from "./components/CreateToDo";
 import EditToDo from "./components/EditToDo";
@@ -12,26 +12,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <nav className="navbar">
-          <img src={logo} width="30" height="30" alt="Test app" />
-          <a href="localhost:3000" target="_blank"></a>
-        </nav>
-        <Link to="/">MERN-Stack ToDo List</Link>
-        <ul>
-          <li>
-            <Link to="/" className="nav-link">
-              ToDos
-            </Link>
-          </li>
-          <li>
-            <Link to="/create" className="nav-link">
-              Create
-            </Link>
-          </li>
-        </ul>
-        <Route path="/" exact component={ToDosList} />
-        <Route path="/edit/:id" component={EditToDo} />
-        <Route path="/create" component={CreateToDo} />
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ToDosList} />
+          <Route path="/edit/:id" component={EditToDo} />
+          <Route path="/create" component={CreateToDo} />
+        </Switch>
       </Router>
     );
   }
---
+++ src/components/NavBar/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from "reactstrap";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Blogger Alt</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/">
+                Home
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink tag={Link} to="/create">
+                Create
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
