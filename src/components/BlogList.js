458316711308a69ee92cb41fa1652290c7b4cba0
Chow Jia Ying <chowjiaying211@gmail.com>
2020-04-16 10:12:38 +0800

    Add blog list for users to choose a blog to go to

---
+++ src/components/BlogList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+const BlogList = (props) => {
+    return (
+        <ListGroup>
+            <ListGroupItem></ListGroupItem>
+        </ListGroup>
+    );
+};
+
+export default BlogList;
