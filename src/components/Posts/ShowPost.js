ccf1878a66b0c1f836be2b3ff0e562922b62ce13
Chow Jia Ying <chowjiaying211@gmail.com>
2020-04-19 15:43:51 +0800

    Allow downloading of blog posts to Word

--- src/components/Posts/ShowPost.js
+++ src/components/Posts/ShowPost.js
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "../../App.css";
+import { Button } from "reactstrap";
 import { Viewer } from "@toast-ui/react-editor";
+import fileDownload from "js-file-download";
 
 class ShowPost extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: "",
-      content: ""
+      content: "",
     };
+    this.viewerRef = React.createRef();
   }
+
+  downloadPost = () => {
+    const header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' " +
+    "xmlns:w='urn:schemas-microsoft-com:office:word' " +
+    "<head><meta charset='utf-8'></head><body>";
+    const footer = "</body></html>";
+    const content = header + this.viewerRef.current.rootEl.current.innerHTML + footer;
+    console.log(content);
+    fileDownload(content, `${this.state.title}.doc`, 'application/vnd.ms-word');
+  };
 
   componentDidMount() {
     axios
       .get(`http://localhost:4000/posts/${this.props.match.params.id}`)
-      .then(response => this.setState({ ...response.data }))
-      .catch(error => console.log(error));
+      .then((response) => this.setState({ ...response.data }))
+      .catch((error) => console.log(error));
   }
 
   render() {
     return (
       <div>
         <h3 className="view-post">{this.state.title}</h3>
-        <Viewer initialValue={this.state.content}></Viewer>
+        <Viewer initialValue={this.state.content} ref={this.viewerRef}></Viewer>
+
+        <Button onClick={this.downloadPost}>Download as Word document</Button>
       </div>
     );
   }
