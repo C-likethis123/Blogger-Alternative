0c13e49d66d5277ff4be909721f2c4b47e7c2eed
Chow Jia Ying <chowjiaying211@gmail.com>
2020-04-19 20:57:58 +0800

    Implement lazy loading in routers and downloading modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/NavBar/Header";
 import {Container} from "reactstrap";
-import CreatePost from "./components/Posts/CreatePost";
-import EditPost from "./components/Posts/EditPost";
-import PostsList from "./components/Posts/PostsList";
-import ShowPost from "./components/Posts/ShowPost";
+const CreatePost = lazy(() => import("./components/Posts/CreatePost.js"));
+const EditPost = lazy(() => import("./components/Posts/EditPost.js"));
+const PostsList = lazy(() => import("./components/Posts/PostsList.js"));
+const ShowPost = lazy(() => import("./components/Posts/ShowPost.js"));
 
 class App extends Component {
   render() {
@@ -16,10 +16,12 @@ class App extends Component {
         <Router>
           <Header />
           <Switch>
+          <Suspense fallback={<div>Loading...</div>}>
             <Route exact path="/" component={PostsList} />
             <Route path="/edit/:id" component={EditPost} />
             <Route path="/create" component={CreatePost} />
             <Route path="/:id" component={ShowPost} />
+            </Suspense>
           </Switch>
         </Router>
       </Container>
