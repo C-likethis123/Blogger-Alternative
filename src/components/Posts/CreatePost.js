8533bcf9d60d3e6ad7e7602fe15ecb2af808a0d3
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-15 13:36:07 +0800

    Change CreatePost to functional components

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -1,133 +1,115 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 
-import {Row, Col} from "reactstrap";
+import { Row, Col } from "reactstrap";
 
 import EditorForm from "../Editor/EditorForm";
 import SaveAlert from "../Alerts/Alerts";
 
 import Paths from '../../constants/paths';
+import { useHistory } from "react-router-dom";
 
-class CreatePost extends Component {
-  constructor(props) {
-    super(props);
+function CreatePost(props) {
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [isDraft, setIsDraft] = React.useState(true);
+  const [savedSuccess, setSavedSuccess] = React.useState(null);
+  const [showAlert, setShowAlert] = React.useState(false);
+  const [id, setId] = React.useState(null);
 
-    this.state = {
-      title: "",
-      content: "",
-      isDraft: true,
-      savedSuccess: null,
-      showAlert: false,
-    };
+  const history = useHistory();
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-    this.onSave = this.onSave.bind(this);
-  }
+  const onChangeTitle = (e) => setTitle(e.target.value);
 
-  onChangeTitle(e) {
-    const title = e.target.value;
-    this.setState({ title });
-  }
-
-  onSubmit(content) {
+  const onSubmit = (content) => {
     const newPost = {
-      title: this.state.title,
-      content: content,
+      title,
+      content,
       isDraft: false,
     };
 
-    if (this.state.id === undefined) {
+    if (id) {
       axios
-        .post("http://localhost:4000/posts/add", newPost)
-        .then((res) => this.setState({ id: res.data.post._id }))
-        .then(() => console.log(this.state));
+        .post(`http://localhost:4000/posts/update/${id}`, newPost)
+        .then((res) => console.log(res.data));
     } else {
       axios
-        .post(`http://localhost:4000/posts/update/${this.state.id}`, newPost)
-        .then((res) => console.log(res.data));
+        .post("http://localhost:4000/posts/add", newPost)
+        .then((res) => setId(res.data.post._id));
     }
-
-    this.props.history.push(Paths.PostsList);
+    history.push(Paths.PostsList);
   }
 
-  onSave(content) {
+  const onSave = (content) => {
     const newPost = {
-      title: this.state.title,
-      content: content,
-      isDraft: this.state.isDraft,
+      title,
+      content,
+      isDraft,
     };
 
-    if (this.state.id === undefined) {
+    if (id) {
+      axios
+        .post(`http://localhost:4000/posts/update/${id}`, newPost)
+        .then((res) => {
+          console.log(res.data);
+          setSavedSuccess(true);
+          setShowAlert(true);
+          setTimeout(() => setShowAlert(false), 2000);
+        })
+        .catch((err) => {
+          console.log(err);
+          setSavedSuccess(false);
+          setShowAlert(true);
+        });
+    } else {
       axios
         .post("http://localhost:4000/posts/add", newPost)
-        .then((res) => this.setState({ id: res.data.post._id }))
-        .then(() => this.setState({ savedSuccess: true, showAlert: true }))
-        .then(() =>
-          setTimeout(() => {
-            this.setState({
-              showAlert: false,
-            });
-          }, 2000)
-        )
+        .then((res) => {
+          setId(res.data.post._id);
+          setSavedSuccess(true);
+          setShowAlert(true);
+          setTimeout(() => setShowAlert(false), 2000)
+        })
         .catch((err) => {
           console.log(err);
-          this.setState({ savedSuccess: false, showAlert: true });
-        });
-    } else {
-      axios
-        .post(`http://localhost:4000/posts/update/${this.state.id}`, newPost)
-        .then((res) => console.log(res.data))
-        .then(() => this.setState({ savedSuccess: true, showAlert: true }))
-        .then(() =>
-          setTimeout(() => {
-            this.setState({
-              showAlert: false,
-            });
-          }, 2000)
-        )
-        .catch((err) => {
-          console.log(err);
-          this.setState({ savedSuccess: false, showAlert: true });
+          setSavedSuccess(false);
+          setShowAlert(true);
         });
     }
   }
 
-  onDelete() {
-    if (this.state.id === undefined) {
-      this.props.history.push(Paths.PostsList);
-    } else {
+  const onDelete = () => {
+    if (id) {
       axios
-        .delete(`http://localhost:4000/posts/${this.state.id}`)
-        .then(() => this.props.history.push(Paths.PostsList));
-    }
+        .delete(`http://localhost:4000/posts/${id}`)
+        .then(() => history.push(Paths.PostsList));
+    } else {
+      history.push(Paths.PostsList);
+    };
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Row>
-          <Col>
-            <h3>Create New Post</h3>
-          </Col>
-          <Col>
-            <SaveAlert
-              isSuccessful={this.state.savedSuccess}
-              showAlert={this.state.showAlert}
-            />
-          </Col>
-        </Row>
-        <EditorForm
-          isEdit={false}
-          onSubmit={this.onSubmit}
-          onChangeTitle={this.onChangeTitle}
-          onDelete={this.onDelete}
-          onSave={this.onSave}
-        />
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Row>
+        <Col>
+          <h3>Create New Post</h3>
+        </Col>
+        <Col>
+          <SaveAlert
+            isSuccessful={savedSuccess}
+            showAlert={showAlert}
+          />
+        </Col>
+      </Row>
+      <EditorForm
+        isEdit={false}
+        onSubmit={onSubmit}
+        onChangeTitle={onChangeTitle}
+        onDelete={onDelete}
+        onSave={onSave}
+      />
+    </React.Fragment>
+  );
 }
 
 export default CreatePost;
