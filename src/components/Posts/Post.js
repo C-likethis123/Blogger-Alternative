a8cfde2a080a4d813763bc45e6a0ece56f4476fe
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-20 22:47:53 +0800

    Revamp UI for posts

---
+++ src/components/Posts/Post.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button as BootstrapButton, Row, Col, Card as BootstrapCard, CardTitle, CardBody, CardSubtitle } from "reactstrap";
+import Paths from '../../constants/paths';
+import { useHistory } from "react-router-dom";
+
+const Button = styled(BootstrapButton)`
+margin-left: 30px;
+`
+
+const Card = styled(BootstrapCard)`
+margin-bottom: 30px;
+`
+
+const ButtonGroup = styled(({ className, children }) => (
+  <Col className={className}>
+    {children}
+  </Col>
+))`
+  text-align: center;
+`
+
+export default function Post(props) {
+  const history = useHistory();
+  const goToEdit = () => history.push(`${Paths.EditPost}/${props.post._id}`);
+  const deletePost = () => props.deletePost(props.post._id);
+  const goToView = () => history.push(`${Paths.Post}/${props.post._id}`);
+  return (
+    <Card>
+      <CardBody>
+        <Row>
+          <Col>
+            <CardTitle tag="h5">
+              {props.post.title || '(Untitled)'}
+            </CardTitle>
+            <CardSubtitle>
+              {props.post.isDraft ? 'Draft' : 'Published'}
+            </CardSubtitle>
+          </Col>
+          <ButtonGroup>
+            <Button color="primary" onClick={goToEdit}>
+              Edit
+            </Button>
+            <Button color="danger" onClick={deletePost}>
+              Delete
+            </Button>
+            <Button color="info" onClick={goToView}>
+              View
+            </Button>
+          </ButtonGroup>
+        </Row>
+      </CardBody>
+    </Card>
+  );
+};
--- src/components/Posts/PostsList.js
+++ src/components/Posts/PostsList.js
@@ -1,29 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-import { Table } from "reactstrap";
 
 import axios from "axios";
 
-import Paths from '../../constants/paths';
 import Title from '../Utils/Title';
-
-const Post = (props) => (
-  <tr>
-    <td width="70%">
-      {props.post.isDraft ? `${props.post.title} (draft)` : props.post.title}
-    </td>
-    <td width="10%">
-      <Link to={`${Paths.EditPost}/${props.post._id}`}>Edit</Link>
-    </td>
-    <td width="10%">
-      <Link onClick={() => props.deletePost(props.post._id)}>Delete</Link>
-    </td>
-    <td width="10%">
-      <Link to={`${Paths.Post}/${props.post._id}`}>View</Link>
-    </td>
-  </tr>
-);
+import Post from './Post';
 
 function PostsList() {
   const [posts, setPosts] = React.useState([]);
@@ -43,23 +23,13 @@ function PostsList() {
   return (
     <>
       <Title>Your Posts</Title>
-      <Table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((currentPost, i) => (
-            <Post
-              post={currentPost}
-              key={i}
-              deletePost={deletePost}
-            />)
-          )}
-        </tbody>
-      </Table>
+      {posts.map((currentPost) => (
+        <Post
+          post={currentPost}
+          key={currentPost._id}
+          deletePost={deletePost}
+        />)
+      )}
     </>
   )
 }
