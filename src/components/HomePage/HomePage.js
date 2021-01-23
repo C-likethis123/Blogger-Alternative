225f5f1d53895b923d070b5d9a692fd486fc85e1
ShengXue97 <simshengxue1997@gmail.com>
2021-01-23 11:59:11 +0800

    test

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "reactstrap";
 import Features from "./Features";
 import SplashPage from "./SplashPage";
+import Login from "./Login";
 
 function HomePage() {
   return (
     <Container>
       <SplashPage />
       <Features />
+      <Login />
     </Container>
   );
 }
