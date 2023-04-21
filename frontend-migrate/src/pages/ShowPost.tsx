2b90e1c37d45cb263b171cce0feb512ff6d566a3
Chow Jia Ying <chowjiaying211@gmail.com>
2023-04-21 21:00:56 +0800

    Migrate ShowPosts

--- frontend-migrate/src/pages/ShowPost.tsx
+++ frontend-migrate/src/pages/ShowPost.tsx
@@ -1,3 +1,26 @@
+import React from "react";
+import axios from "axios";
+import "../../styles/App.css";
+import Viewer from "../components/Viewer";
+import { useParams } from "react-router-dom";
+
 export function Component() {
-    return <div>Show Post</div>
+    const [title, setTitle] = React.useState("");
+    const [content, setContent] = React.useState("");
+    const { id } = useParams<{ id: string }>();
+    const downloadPost = async () => {};
+
+    React.useEffect(() => {
+        axios.get(`/posts/${id}`)
+            .then(({ data: { title, content }}) => {
+                setTitle(title);
+                setContent(content);
+            })
+            .catch((error) => console.log(error));
+    }, [id]);
+    return <div>
+        <h3>{title}</h3>
+        <Viewer value={content} />
+        <button onClick={downloadPost}>Download as Word Document</button>
+    </div>
 }
