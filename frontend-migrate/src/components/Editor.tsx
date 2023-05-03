7ba6330d0207887ffca7843c361d3adf4f789eb0
Chow Jia Ying <chowjiaying211@gmail.com>
2023-05-03 15:47:31 +0800

    Add something to Editor

--- frontend-migrate/src/components/Editor.tsx
+++ frontend-migrate/src/components/Editor.tsx
@@ -9,11 +9,19 @@ interface EditorProps {
     onDelete: () => void; 
     onSave: (content: string) => void;
 }
-export default function Component(props: EditorProps) {
+export default function Component({
+    title,
+    onChangeTitle
+}: EditorProps) {
     const [editorValue, setEditorValue] = useState('');
     const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setEditorValue(event.target.value);
     }
   
-    return <textarea value={editorValue} onChange={onChange}></textarea>
+    return <div>
+        <label htmlFor="title" >Title</label>
+        <input value={title} onChange={onChangeTitle} id="title" name="title"></input>
+        <textarea style={{display: "block"}} value={editorValue} onChange={onChange}></textarea>
+        
+    </div>
 }
