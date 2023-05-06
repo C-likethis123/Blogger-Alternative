4bcac8911937736c3aea6ba98455b8d98d581ba6
Chow Jia Ying <chowjiaying211@gmail.com>
2023-05-06 20:38:52 +0800

    Add tests for authentication

--- server/test/controllers/posts.test.ts
+++ server/test/controllers/posts.test.ts
@@ -1,7 +1,15 @@
 import PostsController from "../../src/controllers/posts";
 import App from "../../src/app";
 import request from "supertest";
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+// stubs authentication
+jest.mock("../../src/middleware/authenticator", () => ({
+    __esModule: true,
+    default: jest.fn((req: Request, res: Response, next: NextFunction) => {
+        return next();
+    }),
+}))
 
 describe("Test Posts Controller", () => {
     let server: express.Application;
@@ -10,13 +18,19 @@ describe("Test Posts Controller", () => {
         server = new App([new PostsController()], 3000).getServer();
     });
 
-    test('GET /posts without authentication', (done) => {
+    test('GET /posts with authentication', (done) => {
         request(server)
             .get("/api/posts")
-            .expect(401)
+            .expect(200)
+            .expect('Content-Type', /json/)
             .end((err, res) => {
                 if (err) return done(err);
+                expect(res.body).toMatchObject([{
+                    title: 'Test',
+                    content: 'Test',
+                    _id: "1",
+                }]);
                 done();
             })
-    });
+    })
 });
