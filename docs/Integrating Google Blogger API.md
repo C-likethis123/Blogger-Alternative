005ea780b89d4a639cdfcb137a53a1a8d81fe2ea
Chow Jia Ying <chowjiaying211@gmail.com>
2023-05-07 18:08:22 +0800

    Implement getting a list of blog posts

--- docs/Integrating Google Blogger API.md
+++ docs/Integrating Google Blogger API.md
@@ -15,6 +15,7 @@ Then attach that access token into the `Authorization` header when we make a Goo
 How do we refresh tokens? When do we refresh tokens?
 
 When the user is authenticated, it passes the access token and refresh token to the client.
+We can set it using `oauth2Client.setCredentials(tokens)`.
 
 The GoogleAPI module has a OAuth2 client that handles retrieval of access tokens, refreshing, and retrying the request. We can use the OAuth2Client to manage access tokens.
 
@@ -31,6 +32,12 @@ This tokens event only occurs in the first authorisation. Once the client has a 
 
 [Refresh tokens best practices](https://stateful.com/blog/oauth-refresh-token-best-practices)
 
+Implementation at the API level
+- create an OAuth2Client
+- anytime the user makes a request, it will send its user id.
+- find access token and refresh token from the database
+- set OAuth2Client with the database
+- create an event listener to listen for the 'tokens' event and store refresh token in the user's database
 
 ## Library of choice
 We can use axios to make requests and parse responses ourselves.
@@ -89,7 +96,27 @@ console.log(createResponse.data);
 
 ## Version
 
-There are two versions - Google Blogger API v2 and v3.
+There are two versions - Google Blogger API v2 and v3. I'm using Google Blogger API v3.
+
+## Errors
+
+### Authentication errors 
+I saw the "UserInfo" error which indicated the credentials did not have the correct permissions to use the Google Blogger API
+Possible reasons why:
+- not enabling required scopes - blogger scope has to be accompanied with the profile
+- setting the app to a "in production" instead of "testing"
+
+### API errors
+
+`Blogs#listByUser`: I got a 404 error when using the ID accompanied from the Google Account.
+- Note that the blogger ID is different from the user ID.
+- important fields for this API: blog.items
+- important field for a blog: blog.id, blog.name, pages
+
+
+### APIs exposed to frontend
+
+This is detailed in [API Spec](./API%20spec.md)
 
 ## Resources
 
--- server/src/controllers/posts.ts
+++ server/src/controllers/posts.ts
@@ -1,20 +1,25 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import Controller from './controller';
 import checkAuthenticated from '../middleware/authenticator';
-import {google } from 'googleapis';
-
-const blogger = google.blogger('v3');
+import { google } from 'googleapis';
+import type { Credentials } from 'google-auth-library';
 
 type User = {
    id: string;
-   token: string;
+   tokens: Credentials
 }
+
+/*
+What do I need to test here?
+- test that I can retrieve the blogs
+- need to stub blogger API client
+*/
 class PostsController implements Controller {
    public router = express.Router();
 
    private posts = [{
       title: 'Test',
-      content: 'Test', 
+      content: 'Test',
       _id: '1',
    }]
    constructor() {
@@ -22,15 +27,34 @@ class PostsController implements Controller {
          res.json(this.posts);
       });
       this.router.get('/api/blogs', checkAuthenticated, async (req: Request, res: Response) => {
-         const {id, token} = req.user as User;
          // from user ID, retrieve access token directly
+         const { tokens } = req.user as User;
 
          // initialise a GoogleOAuth client
+         const oauth2Client = new google.auth.OAuth2(
+            process.env.GOOGLE_OAUTH_CLIENT_ID,
+            process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+            "http://localhost:8000/oauth/redirect/google",
+         );
+         oauth2Client.setCredentials(tokens);
+
+         const blogger = google.blogger({
+            version: "v3",
+            auth: oauth2Client
+         })
 
          // make this request
          const blogs = await blogger.blogs.listByUser({
-            userId: id,
+            userId: "self",
+            fields: "items(id,status,name)",
          });
+         if (blogs.status === 200) {
+            const listOfBlogs = blogs.data.items;
+            console.log(listOfBlogs);
+            return res.status(200).json(listOfBlogs);
+         }
+         //TODO: proper error handling
+         return res.status(400).json(blogs);
       });
    }
 }
