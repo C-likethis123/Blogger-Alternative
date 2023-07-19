12d7d5ad260627a904bd9259b3c86f53b627442b
Chow Jia Ying <chowjiaying211@gmail.com>
2023-07-19 23:05:36 +0800

    Add MongoDB database

---
+++ server/src/db/mongo.ts
@@ -0,0 +1,27 @@
+import mongoose, { Mongoose } from "mongoose";
+/**
+ * TODO: Improve this class. Not sure what I can do to improve it, it looks like it should be a singleton pattern.
+ */
+class MongoConnection {
+  private static db: Mongoose;
+
+  static isInitialized(): boolean {
+    return this.db !== undefined;
+  }
+
+  static async getDatabase(): Promise<Mongoose> {
+    if (this.isInitialized()) {
+        return this.db;
+    }
+
+    // Initialize the connection.
+    try {
+        this.db = await mongoose.connect(process.env.MONGODB_URL);
+        return this.db;
+    } catch (e) {
+        throw new Error("Database cannot be initialised - " + e.message);
+    }
+  }
+}
+
+export default MongoConnection;
