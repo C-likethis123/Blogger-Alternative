25bb807ca818afc9867b28a5c44943a27f4caa7f
Chow Jia Ying <chowjiaying211@gmail.com>
2024-03-09 14:32:15 +0800

    [fix] fix bug where users cannot log out of Safari

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -4,12 +4,12 @@ import { Schema, Model, model } from "mongoose";
  * Represents logged in users
  */
 interface User {
-    email: string;
+    id: string;
     access_token: string,
     refresh_token: string,
 }
 const UserSchema: Schema<User> = new Schema({
-    email: String, // make this unique?
+    id: String, // make this unique?
     // tokens from OAuth
     access_token: String,
     refresh_token: String,
