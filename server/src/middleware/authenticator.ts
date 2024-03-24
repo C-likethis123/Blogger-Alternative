beb3f8c90145bcabe28afded1558ab72dca24dac
Chow Jia Ying <chowjiaying211@gmail.com>
2024-03-24 20:50:39 +0800

    [fix] use a single oauth2Client per request

--- server/src/middleware/authenticator.ts
+++ server/src/middleware/authenticator.ts
@@ -3,16 +3,18 @@ import { google } from 'googleapis';
 
 export default function checkAuthenticated(req: Request, res: Response, next: NextFunction) {
     if (req.isAuthenticated()) {
-        // initialise a GoogleOAuth client
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GOOGLE_OAUTH_CLIENT_ID,
-            process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-            "http://localhost:8000/oauth/redirect/google",
-        );
-        const { tokens } = req.user;
-        oauth2Client.setCredentials(tokens);
-        req.oauth2Client = oauth2Client;
+        // initialise a GoogleOAuth client if it's not present
+        if (!req.oauth2Client) {
+            const oauth2Client = new google.auth.OAuth2(
+                process.env.GOOGLE_OAUTH_CLIENT_ID,
+                process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+                "http://localhost:8000/oauth/redirect/google",
+            );
+            const { tokens } = req.user;
+            oauth2Client.setCredentials(tokens);
+            req.oauth2Client = oauth2Client;
+        }
         return next();
     }
-    return res.status(401).json({message: "User is not authenticated!"});
+    return res.status(401).json({ message: "User is not authenticated!" });
 }
